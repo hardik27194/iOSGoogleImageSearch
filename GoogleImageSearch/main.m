06fdae90c0f4ced1b6ec46fa05e59599d083640e
Daniel Trostli <daniel.trostli@gmail.com>
2014-10-21 14:30:43 -0700

    Initial Commit

---
+++ GoogleImageSearch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GoogleImageSearch
+//
+//  Created by Daniel on 10/21/14.
+//  Copyright (c) 2014 DT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ GoogleImageSearchTests/GoogleImageSearchTests.m
@@ -0,0 +1,40 @@
+//
+//  GoogleImageSearchTests.m
+//  GoogleImageSearchTests
+//
+//  Created by Daniel on 10/21/14.
+//  Copyright (c) 2014 DT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface GoogleImageSearchTests : XCTestCase
+
+@end
+
+@implementation GoogleImageSearchTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
